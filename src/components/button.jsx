2c596ed92ac10dffa82c648f05ec97a83f647770
Jaime Chochi rieba <macjaime@MBP-de-Jaime.home>
2023-07-08 18:18:38 +0200

    F

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,19 +1,73 @@
-import React from 'react';
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
 
 
-export function Button () {
- 
-  let boton = document.querySelector(".btn");
-  let div = document.querySelector(".divPadre")
+export function Button ({  setColor, color }) {
 
-  const [counter, setCounter] = useState(0);
-  const handleClick = () => setCounter(5);
-
-	return 
   
+    
+
+    const changeColor = () => {
+      setColor(color === 'light' ? 'dark' : 'light');
+    };
+
+    
+    return (
+      <div>
+        <button  onClick={changeColor}>Cambiar Color</button>
+        
+      </div>
+     );
   
-  
+
 }
-Button();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+
+// export function Button() {
+//   const ColorChanger = () => {
+//     const [color, setColor] = useState('red');
+
+//     const changeColor = () => {
+//       setColor(color === 'red' ? 'blue' : 'red');
+//     };
+
+//     useEffect(() => {
+//       // Actualizar el color del fondo del <div> cada vez que 'color' cambie
+//       const myDiv = document.getElementById('myDiv');
+//       myDiv.style.backgroundColor = color;
+//     }, [color]);
+
+//     return (
+//       <div>
+//         <button onClick={changeColor}>Cambiar Color</button>
+//         <div id="myDiv" style={{ width: '200px', height: '200px' }}></div>
+//       </div>
+//     );
+//   };
+
+//   return <ColorChanger />;
+// }
+
+
 
--- src/components/compon.jsx
+++ src/components/compon.jsx
@@ -1,30 +1,19 @@
-import React from "react";
-import { useEffect, useState } from "react"
+
+import { Button } from "./button"
+import { changeColor } from "./button";
+import React, { useState } from 'react';
 
 export function Component () {
 
-        // const [dark, setDark] = useState(false);
-        // const HandleCLick = () => {
-        //     setDark(true);
-        //    return (
-        //     <>
-            
-        //     <button className= "handleButton" onClick = {HandleCLick}></button>
-        //     </>
-        //    )
-        //      const [dark,setDark] = useState(false)
-        // const add = () => setDark( prev => prev += true )
-        // console.log(add);
-    
-    
-        // ${dark === true ? 'dark' : "" }
+    const [color, setColor] = useState('light');
 
     return (
-        <div className= "divPadre">
+        <div id="myDiv" className= {`divPadre ${color}`}>
             <h1 className="title">TITULO</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic exercitationem ratione possimus in pariatur, magnam, ullam quae praesentium corrupti quasi ipsum veniam saepe accusantium temporibus! Rerum exercitationem ad accusantium optio.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur voluptate magni expedita vel pariatur nihil vitae sed distinctio porro officiis harum consequuntur, illum, dolor perspiciatis alias recusandae obcaecati quaerat laborum.</p>
-            <button className="btn" >Dark</button>
+             
+            <Button setColor={setColor} color={color}/>
             
         </div>
     )
